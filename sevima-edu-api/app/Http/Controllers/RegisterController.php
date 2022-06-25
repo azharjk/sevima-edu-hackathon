4d5fabb7a32755ddfc2a384d801172e6bcff23dd
azharjk <azhr.project@gmail.com>
2022-06-25 18:24:29 +0700

    Api: Add register endpoint

---
+++ sevima-edu-api/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use App\Http\Resources\TokenResource;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        // FIXME: Handle already created username
+        $user = User::create($request->all());
+        $token = $user->createToken('token-auth')->plainTextToken;
+        return new TokenResource($token);
+    }
+}
