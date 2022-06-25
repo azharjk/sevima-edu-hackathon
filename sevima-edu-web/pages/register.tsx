20dfec328560afe9f98dfbfc0ad6474ba2ce550e
azharjk <azhr.project@gmail.com>
2022-06-25 11:12:54 +0700

    Web: Add register page

--- sevima-edu-web/pages/register.tsx
+++ sevima-edu-web/pages/register.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 import type { NextPage } from "next";
 
-const Login: NextPage = () => {
+const Register: NextPage = () => {
   return (
     <div className="w-screen h-screen flex justify-center">
       <div className="mt-[5rem] h-min max-w-[27rem] w-full p-4 sm:border">
         <header className="flex justify-center">
           <span>
-            <span className="font-semibold">Sevima Edu</span> - Login
+            <span className="font-semibold">Sevima Edu</span> - Register
           </span>
         </header>
         <form action="" className="mt-4 flex flex-col gap-3">
@@ -28,15 +28,23 @@ const Login: NextPage = () => {
               placeholder="Enter your password"
             />
           </div>
+          <div className="flex flex-col">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm-password"
+              placeholder="Repeat your password"
+            />
+          </div>
           <button className="w-full px-4 py-2 bg-primary hover:bg-primary-darker text-white">
-            Login
+            Register
           </button>
         </form>
         <div className="flex flex-col items-center gap-3 mt-4">
           <span className="uppercase font-semibold">Or</span>
           <span>
-            Do not have an account?{" "}
-            <Link href="/register">
+            Already have an account?{" "}
+            <Link href="/login">
               <a
                 className="text-primary hover:text-primary-darker hover:underline"
                 href=""
@@ -51,4 +59,4 @@ const Login: NextPage = () => {
   );
 };
 
-export default Login;
+export default Register;
