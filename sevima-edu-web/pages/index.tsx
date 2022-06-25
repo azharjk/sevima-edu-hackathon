087a6b3c5ddc7451d3d5371ac1d1b299803b601c
azharjk <azhr.project@gmail.com>
2022-06-25 22:23:10 +0700

    Web: Consume forums endpoint

--- sevima-edu-web/pages/index.tsx
+++ sevima-edu-web/pages/index.tsx
@@ -1,15 +1,61 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 import ForumCard from "../components/ForumCard";
 
+interface User {
+  username: string;
+}
+
+interface Forum {
+  id: number;
+  title: string;
+  author: User;
+}
+
+interface ForumResponse {
+  data: Forum[];
+}
+
+// FIXME: Use server side nextjs authentication.
+//        The implementation now preventing to
+//        fetch server side props.
+
 const Home: NextPage = () => {
+  const [forums, setForums] = useState<Forum[]>([]);
+
+  useEffect(() => {
+    const fetchForums = async () => {
+      const token = localStorage.getItem("auth_token");
+
+      if (token === null) {
+        // FIXME: Handle unauthorized case
+        alert("Unauthorized");
+        return;
+      }
+
+      const res = await fetch("http://localhost:8000/api/user/forums", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const { data } = (await res.json()) as ForumResponse;
+
+      setForums(data);
+    };
+
+    fetchForums();
+  }, []);
+
   return (
     <div className="flex justify-center">
       <main className="p-4 w-full max-w-[70rem]">
         <ul className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
-          {Array.apply(null, Array(7)).map((x, idx) => (
-            <li key={idx}>
-              <ForumCard title="IPA TEST DOAng" author="Dr Hill" id={idx} />
+          {forums.map(({ id, title, author }) => (
+            <li key={id}>
+              <ForumCard title={title} author={author.username} id={id} />
             </li>
           ))}
         </ul>
