64795aea3469424769e74952876588f27236b66e
azharjk <azhr.project@gmail.com>
2022-06-25 14:20:04 +0700

    Web: Add forum detail page

--- sevima-edu-web/components/ForumCard.tsx
+++ sevima-edu-web/components/ForumCard.tsx
@@ -1,15 +1,22 @@
+import Link from "next/link";
+
 interface ForumCardProps {
   title: string;
   author: string;
+  id: number;
 }
 
-const ForumCard: React.FC<ForumCardProps> = ({ title, author }) => {
+const ForumCard: React.FC<ForumCardProps> = ({ title, author, id }) => {
   return (
     <div className="border p-3 h-[10rem] hover:shadow-md">
       <div className="flex flex-col gap-2">
-        <a href="">
-          <span className="font-semibold text-primary underline">{title}</span>
-        </a>
+        <Link href={`/forums/${id}`}>
+          <a>
+            <span className="font-semibold text-primary underline">
+              {title}
+            </span>
+          </a>
+        </Link>
         <span className="text-xs text-icon-color">{author}</span>
       </div>
     </div>
--- sevima-edu-web/pages/index.tsx
+++ sevima-edu-web/pages/index.tsx
@@ -1,24 +1,20 @@
 import type { NextPage } from "next";
 
-import Header from "../components/Header";
 import ForumCard from "../components/ForumCard";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Header />
-      <div className="flex justify-center">
-        <main className="p-4 w-full max-w-[70rem]">
-          <ul className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
-            {Array.apply(null, Array(7)).map((x, idx) => (
-              <li key={idx}>
-                <ForumCard title="IPA TEST DOAng" author="Dr Hill" />
-              </li>
-            ))}
-          </ul>
-        </main>
-      </div>
-    </>
+    <div className="flex justify-center">
+      <main className="p-4 w-full max-w-[70rem]">
+        <ul className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
+          {Array.apply(null, Array(7)).map((x, idx) => (
+            <li key={idx}>
+              <ForumCard title="IPA TEST DOAng" author="Dr Hill" id={idx} />
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 };
 
